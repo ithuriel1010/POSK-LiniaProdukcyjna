f59a08f00b576754fa0942ef06698ce523c96f2d
Aleksandra Wędołowska <ithuriel1010@gmail.com>
2020-01-08 23:43:12 +0100

    Cake is produced and 1/4 cakes are chocolate

--- LiniaProdukcyjnaApp/Form1.Designer.cs
+++ LiniaProdukcyjnaApp/Form1.Designer.cs
@@ -28,13 +28,106 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(383, 350);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(181, 170);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(383, 284);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(181, 170);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(383, 199);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(181, 170);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(383, 126);
+            this.pictureBox4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(181, 170);
+            this.pictureBox4.TabIndex = 4;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.PictureBox4_Click);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Location = new System.Drawing.Point(383, 0);
+            this.pictureBox5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(181, 109);
+            this.pictureBox5.TabIndex = 5;
+            this.pictureBox5.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(425, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Cake Factory!";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(981, 576);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- LiniaProdukcyjnaApp/Form1.cs
+++ LiniaProdukcyjnaApp/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +13,117 @@ namespace LiniaProdukcyjnaApp
 {
     public partial class Form1 : Form
     {
+        int licznik = 0;
+        Image tort = Image.FromFile("D:/Studia/Semestr V/Współczesne języki programowania/Projekt/Sweets/PNG/tort.png");
+        int random;
         public Form1()
         {
             InitializeComponent();
+            pictureBox5.BackColor = Color.Black;
+            var thread = new Thread(StartCounting);
+            thread.IsBackground = true;
+            thread.Start();
+
+
+        }
+
+        private delegate void SetImageDelegate(Image path);
+
+        private void SetImagePicture4(Image path)
+        {
+            pictureBox4.Visible = true;
+            pictureBox4.Image = path;
+            pictureBox1.Visible = false;
+            pictureBox2.Visible = false;
+            pictureBox3.Visible = false;
+        }
+        private void SetImagePicture2(Image path)
+        {
+            pictureBox2.Visible = true;
+            pictureBox2.Image = path;
+            pictureBox4.Visible = false;
+        }
+        private void SetImagePicture3(Image path)
+        {
+            pictureBox3.Visible = true;
+            pictureBox3.Image = path;
+            pictureBox4.Visible = false;
+            pictureBox2.Visible = false;
+        }
+        private void SetImagePicture1(Image path)
+        {
+            pictureBox1.Visible = true;
+            pictureBox1.Image = path;
+            pictureBox4.Visible = false;
+            pictureBox2.Visible = false;
+            pictureBox3.Visible = false;
+        }
+        private void DoNothing(Image path)
+        {
+            pictureBox1.Visible = false;
+            pictureBox4.Visible = false;
+            pictureBox2.Visible = false;
+            pictureBox3.Visible = false;
+        }
+
+
+        private void StartCounting()
+        {
+
+            while(true)
+            {
+                licznik++;
+                Thread.Sleep(2000);
+
+                if (licznik == 1)
+                {
+                    random = GetRandomNumber(0, 4);
+                    if(random==2)
+                    {
+                        tort= Image.FromFile("D:/Studia/Semestr V/Współczesne języki programowania/Projekt/Sweets/PNG/tort2.png");
+                    }
+                    else
+                    {
+                        tort = Image.FromFile("D:/Studia/Semestr V/Współczesne języki programowania/Projekt/Sweets/PNG/tort.png");
+                    }
+                    pictureBox4.Invoke(new SetImageDelegate(SetImagePicture4), tort);
+                }
+                else if (licznik == 2)
+                {
+                    pictureBox2.Invoke(new SetImageDelegate(SetImagePicture2), tort);
+                }
+                else if (licznik == 3)
+                {
+                    pictureBox3.Invoke(new SetImageDelegate(SetImagePicture3), tort);
+                }
+                else if (licznik == 4)
+                {
+                    pictureBox1.Invoke(new SetImageDelegate(SetImagePicture1), tort);
+                }
+                else if (licznik == 5)
+                {
+                    pictureBox1.Invoke(new SetImageDelegate(DoNothing), tort);
+                    licznik = 0;
+                }
+            }
+            
+
+
+            //thread.Start();
+        }
+
+        private static readonly Random getrandom = new Random();
+        public static int GetRandomNumber(int min, int max)
+        {
+            lock (getrandom) // synchronize
+            {
+                return getrandom.Next(min, max);
+            }
+        }
+
+        private void PictureBox4_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
